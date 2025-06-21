1f75c32be9212daa4f80f5ea321d87ee5ae9a3b8
sonasmacrae98 <167984163+sonasmacrae98@users.noreply.github.com>
2025-06-21 11:17:31 +0200

    Update main.tf
    defined the vnet and subnet

--- iac/main.tf
+++ iac/main.tf
@@ -18,3 +18,17 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "waracle-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "web-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
