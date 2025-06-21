cd4edf15995e8757729f9b555171dbdd93c97995
sonasmacrae98 <167984163+sonasmacrae98@users.noreply.github.com>
2025-06-21 11:12:14 +0200

    Update variables.tf
    Defined the outputs used in the main.tf file

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,1 +1,13 @@
+variable "resource_group_name" {
+  description = "Azure resource group name"
+  type        = string
+  default     = "waracle-rg"
+}
+
+variable "location" {
+  description = "Azure region for all resources"
+  type        = string
+  default     = "UK South"
+}
+
 
