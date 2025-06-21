68e4d48227eec6e6a7e6568932b6fbedde0176b7
sonasmacrae98 <167984163+sonasmacrae98@users.noreply.github.com>
2025-06-21 11:52:37 +0200

    Update cloud-init.sh
    Now injecting the html file here, as it's a cleaner solution

--- iac/scripts/cloud-init.sh
+++ iac/scripts/cloud-init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 apt-get update
 apt-get install -y nginx
-echo "<h1>Hello there</h1>" > /var/www/html/index.html
+echo "${html_content}" > /var/www/html/index.html
 systemctl enable nginx
 systemctl start nginx
-
