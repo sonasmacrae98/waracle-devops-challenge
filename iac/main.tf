8999824e2d5f2ddd8cc392dfa1f9023d1d027916
sonasmacrae98 <167984163+sonasmacrae98@users.noreply.github.com>
2025-06-21 11:07:17 +0200

    Update main.tf
    Defined Terraform provider block and resource group.

--- iac/main.tf
+++ iac/main.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.4.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
