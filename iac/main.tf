2b681863f66eeafa60e4359b58e96d703835195a
sonasmacrae98 <167984163+sonasmacrae98@users.noreply.github.com>
2025-06-21 11:27:41 +0200

    Update main.tf
    Created a network interface and public IP

--- iac/main.tf
+++ iac/main.tf
@@ -55,4 +55,25 @@ resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
+resource "azurerm_network_interface" "nic" {
+  name                = "web-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.web_public_ip.id
+  }
+}
+
+resource "azurerm_public_ip" "web_public_ip" {
+  name                = "web-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
 
