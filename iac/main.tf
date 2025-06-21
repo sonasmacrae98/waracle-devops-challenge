22ccc1b73fff7d34cb9474406e91fdc1198bba3a
sonasmacrae98 <167984163+sonasmacrae98@users.noreply.github.com>
2025-06-21 12:36:28 +0200

    Update main.tf
    Not sure how the resource group got deleted, this is now added again

--- iac/main.tf
+++ iac/main.tf
@@ -21,6 +21,11 @@ data "template_file" "cloud_init" {
   }
 }
 
+resource "azurerm_resource_group" "rg" {
+  name     = "waracle-rg"
+  location = var.location
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "waracle-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -30,7 +35,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "subnet" {
   name                 = "web-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = a.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
@@ -38,7 +43,7 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_network_security_group" "nsg" {
   name                = "web-nsg"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = a.rg.name
 
   security_rule {
     name                       = "allow-http"
@@ -61,7 +66,7 @@ resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
 resource "azurerm_network_interface" "nic" {
   name                = "web-nic"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = a.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -74,7 +79,7 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_public_ip" "web_public_ip" {
   name                = "web-pip"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = a.rg.name
   allocation_method   = "Dynamic"
   sku                 = "Basic"
 }
@@ -82,7 +87,7 @@ resource "azurerm_public_ip" "web_public_ip" {
 resource "azurerm_linux_virtual_machine" "web_vm" {
   name                = "web-vm"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = a.rg.name
   size                = "Standard_B1s"
   admin_username      = "azureuser"
   network_interface_ids = [
